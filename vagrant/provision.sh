d55cf99fa51f2ff5482dd2adbf495749530eaa38
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-05-14 16:06:23 -0500

    Address issues raised by @BrianHicks
    * Use `easy_install pip`
    * Install mi requirements in a virtualenv

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,10 +1,16 @@
-  yum makecache
-  yum install -y gcc python-devel
-  curl --silent https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-  python /tmp/get-pip.py 2>/dev/null
-  cd /vagrant && pip install -r requirements.txt
+yum makecache
+yum install -y gcc python-devel python-virtualenv libselinux-python
+easy_install pip
+
+cd /tmp
+virtualenv env --system-site-packages
+env/bin/pip install -r /vagrant/requirements.txt
+source env/bin/activate
+
+cd /vagrant
 
 if [ ! -f /vagrant/security.yml ]; then
-  cd /vagrant && ./security-setup --enable=false
+  ./security-setup --enable=false
 fi
-cd /vagrant && ansible-playbook vagrant.yml -e @security.yml -i /vagrant/vagrant/vagrant-inventory
+
+ansible-playbook vagrant.yml -e @security.yml -i /vagrant/vagrant/vagrant-inventory
